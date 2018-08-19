ba0c6f276f1aae44bec00fbd2b7a795efd1c3cd4
dias <guilhermedude.7@gmail.com>
2018-08-19 17:31:47 -0300

    Aperfeiçoamento estético

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AppRoutingModule } from './app.routing';
 import { NavbarModule } from './shared/navbar/navbar.module';
 import { FooterModule } from './shared/footer/footer.module';
 import { SidebarModule } from './sidebar/sidebar.module';
-import { LbdModule } from './lbd/lbd.module';
 import {CommonModule} from "@angular/common";
 import {AuthModule} from "./user/auth/auth.module";
 
@@ -56,7 +55,6 @@ import { UpdateEventComponent } from './evento/update-event/update-event.compone
     SidebarModule,
     RouterModule,
     AppRoutingModule,
-    LbdModule,
     CommonModule,
     AuthModule,
   ],
