fc630989b1d06da6ab4437de552fef10fa8c7ad7
dias <guilhermedude.7@gmail.com>
2018-08-23 19:43:56 -0300

    listando usuários na frequência

--- src/app/evento/controle-frequencia/controle-frequencia.component.ts
+++ src/app/evento/controle-frequencia/controle-frequencia.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-controle-frequencia',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./controle-frequencia.component.scss']
 })
 export class ControleFrequenciaComponent implements OnInit {
+  users = [];
+  userObj: object = {};
 
-  constructor() { }
+  constructor(
+    private http: Http,
+  ) { }
+  
+  listUsers = function() {
+    this.http.get('http://sciec.test/admin/user/index').subscribe(
+        (res: Response) => {
+            this.users = res.json();  
+            console.log(res);
+        }
+    );
+};
 
   ngOnInit() {
+    this.listUsers();
   }
 
 }
